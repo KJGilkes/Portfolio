49a337f9a5c7d69fbf5e3ab3d90da6895ecbf510
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2017-03-05 15:17:47 -0500

    Added basic routing throughthe action variable

--- PHP/controller/main.php
+++ PHP/controller/main.php
@@ -4,5 +4,19 @@
 
   $view = new View();
 
-  $view->getView('view/home.php');
+  if(isset($_GET['action'])){
+    $action = $_GET['action'];
+  }else {
+    $action = '';
+  }
+
+  if($action == 'Home' || $action == ''){
+    $view->getView('view/home.php');
+  }else if($action == 'about'){
+    $view->getView('view/about.php');
+  }else if($action == 'contact'){
+    $view->getView('view/contact.php');
+  }else{
+    $view->getView('view/notFound.php');
+  }
  ?>
