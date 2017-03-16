d5d05583394d828d198b48c7d37edc2f8ab5a07c
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2017-03-16 19:10:44 -0400

    Fixed the application to use a newly specified object created from the api response object but now need to figure out why the page reloads when using the submit button

--- Javascript/js/call.js
+++ Javascript/js/call.js
@@ -31,19 +31,14 @@ request.onreadystatechange = () => {
         if (request.status === 200) {
             //Parses through the request object's responseText property
             let currencyRates = JSON.parse(request.responseText);
-
-
+            //Creates a new variable to store the rates from the xml response
+            let rateObject = currencyRates['rates'];
             //Changes the text inside of the p tag for all rates to show the rates from the api
-            allRates.innerHTML = "These rates are from the base currency of euro's " + JSON.stringify(currencyRates['rates']);
+            allRates.innerHTML = "These rates are from the base currency of euro's " + JSON.stringify(rateObject);
             //Declares the base currency to exchange currency to and from
             fx.base = "USD";
-            //Sets the object of currency values to a new variable
-            let currencyValues = Object.values(currencyRates['rates']);
-            for(property in currencyValues){
-              console.log(currencyValues[property]);
-            }
             //Sets each of the rates for the library to exchange between
-            fx.rates = currencyValues;
+            fx.rates = rateObject;
         } else {
             allRates.innerHTML = 'An error occurred: ' + request.status + ' ' + request.statusText;
         }
