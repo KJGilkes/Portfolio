d406d250db3539403fa9930223f96c0a7d218ab4
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2017-03-18 19:52:46 -0400

    Added two new variables to take in user inputed values and placed them insde the constructor function to make a new man

--- Javascript_Components/js/main.js
+++ Javascript_Components/js/main.js
@@ -1,7 +1,10 @@
+let userInputName = document.getElementById('userManName').value;
+let userInputAge = document.getElementById('userManAge').value;
+
 function Man(name, age){
-  this.name  = name;
+  this.name = name;
   this.age = age;
 }
 
-let guy = new Man('Alfred Hitchcock', '56');
+let guy = new Man(userInputName, userInputAge);
 console.log(guy);
