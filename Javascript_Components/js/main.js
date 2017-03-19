570783eebfb2846ab37c915f54e220911adcc7fd
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2017-03-19 18:20:09 -0400

    Added contextual comments to the main js file to explain all that it is doing

--- Javascript_Components/js/main.js
+++ Javascript_Components/js/main.js
@@ -1,14 +1,21 @@
+//Creates a variable with reference to the button element from index.html with an id of submitButton
 let button = document.getElementById('submitButton');
 
+//Constructor function
+//This constructor function acts as blueprints for creating an object with each of its parameters as properties
 function Man(name, age){
+  //Takes in the name parameter and sets it to the name property of the object
   this.name = name;
+  //Takes in the age parameter and sets it to the age property of the object
   this.age = age;
 }
 
 //Adds an event listener that listens for a button click
 button.addEventListener('click', function(){
+  //Creates a variable with reference to the userInputName element from index.html with an id of manName
   let userInputName = document.getElementById('manName').value;
+  //Creates a variable with reference to the userInputAge element from index.html with an id of manAge
   let userInputAge = document.getElementById('manAge').value;
+  //Creates a new object called guy with a name and age taken from the user's input
   let guy = new Man(userInputName, userInputAge);
-  console.log(guy);
 });
