abb6a6681beca41854a0f96d62ac52ac5e88fb98
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2017-03-19 18:01:51 -0400

    Moved the user input variable declarations into the event listener to provide scope for the function

--- Javascript_Components/js/main.js
+++ Javascript_Components/js/main.js
@@ -1,5 +1,3 @@
-let userInputName = document.getElementById('manName').value;
-let userInputAge = document.getElementById('manAge').value;
 let button = document.getElementById('submitButton');
 
 function Man(name, age){
@@ -7,7 +5,10 @@ function Man(name, age){
   this.age = age;
 }
 
+//Adds an event listener that listens for a button click
 button.addEventListener('click', function(){
+  let userInputName = document.getElementById('manName').value;
+  let userInputAge = document.getElementById('manAge').value;
   let guy = new Man(userInputName, userInputAge);
-  console.log(userInputName, userInputAge);
+  console.log(guy);
 });
